fba1b08c9166ae38583a7186717ad9942348bedd
leeminkyoung <4187503@gmail.com>
2018-01-03 15:24:28 +0900

    signup reg

--- sa/forms.py
+++ sa/forms.py
@@ -3,19 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 
-# class CreateUserForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-#
-#     class Meta:
-#         model = User
-#         fields = ("username", "email", "password1", "password2")
-#
-#     def save(self, commit=True):
-#         user = super(RegisterUserForm, self).save(commit=False)
-#         user.email = self.cleaned_data["email"]
-#         if commit:
-#             user.save()
-#         return user
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "email", "pwd", "pwd2")
+
+    def save(self, commit=True):
+        user = super(RegisterUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
 
 
